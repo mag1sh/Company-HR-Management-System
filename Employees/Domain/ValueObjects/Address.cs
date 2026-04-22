162e4e1bd8f9a1d9e4625bf8ea5c7fc9f4942cad
Sverlio <PCK212@K212>
2026-04-22 09:37:22 +0300

    Address

--- Employees/Domain/ValueObjects/Address.cs
+++ Employees/Domain/ValueObjects/Address.cs
@@ -8,5 +8,11 @@ namespace CompanyHRManagementSystem.Employees.Domain.ValueObjects
 {
     public class Address
     {
+        public string Country { get; private set; }
+        public string City { get; private set; }
+        public string PostalCode { get; private set; }
+        public string Street { get; private set; }
+        public string StreetNumber { get; private set; }
+
     }
 }
