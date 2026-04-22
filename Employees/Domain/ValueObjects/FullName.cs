3697b0c2e9bc0dcc3b0af0d2de102866b58911a3
PCK207 <PCK207@K207>
2026-04-22 09:42:57 +0300

    implementirane na klas fullName

--- Employees/Domain/ValueObjects/FullName.cs
+++ Employees/Domain/ValueObjects/FullName.cs
@@ -8,5 +8,17 @@ namespace CompanyHRManagementSystem.Employees.Domain.ValueObjects
 {
     public class FullName
     {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public FullName(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
     }
 }
