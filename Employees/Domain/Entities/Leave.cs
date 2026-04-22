6f9f2245d5859db7f18609046223b636a4913ec1
PCK207 <PCK207@K207>
2026-04-22 10:18:55 +0300

    dobavqne na publichni poleta v klas Leave

--- Employees/Domain/Entities/Leave.cs
+++ Employees/Domain/Entities/Leave.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using CompanyHRManagementSystem.Employees.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CompanyHRManagementSystem.Employees.Domain.Entities
 {
-    internal class Leave
+    public class Leave
     {
+        public int Id { get; private set; }
+        public int EmployeeId { get; private set; }
+        public LeaveType LeaveType { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public int CountDays { get; set; }
+        public LeaveStatus Status { get; private set; }
+
     }
 }
