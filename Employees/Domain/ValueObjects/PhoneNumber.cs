2af33815b6c0a2ec200f58e82d71b57f295356e7
PCK207 <PCK207@K207>
2026-04-22 09:44:21 +0300

    implementirane na klas PhoneNumber

--- Employees/Domain/ValueObjects/PhoneNumber.cs
+++ Employees/Domain/ValueObjects/PhoneNumber.cs
@@ -8,5 +8,15 @@ namespace CompanyHRManagementSystem.Employees.Domain.ValueObjects
 {
     public class PhoneNumber
     {
+        public string Number { get; }
+        public PhoneNumber(string number)
+        {
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return Number;
+        }
     }
 }
