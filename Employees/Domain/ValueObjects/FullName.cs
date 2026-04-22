d33da4af5e054b0080c7894d1361e3ee86c87d7b
PCK207 <PCK207@K207>
2026-04-22 09:43:40 +0300

    dobaveni validacii v klas FullName

--- Employees/Domain/ValueObjects/FullName.cs
+++ Employees/Domain/ValueObjects/FullName.cs
@@ -13,6 +13,10 @@ namespace CompanyHRManagementSystem.Employees.Domain.ValueObjects
 
         public FullName(string firstName, string lastName)
         {
+            if (string.IsNullOrWhiteSpace(firstName))
+                throw new ArgumentException("First name cannot be empty.");
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException("Last name cannot be empty.");
             FirstName = firstName;
             LastName = lastName;
         }
