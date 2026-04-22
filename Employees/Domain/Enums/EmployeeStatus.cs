2c52222342c0338c806208ba9cc86a0d21acb053
PCK207 <PCK207@K207>
2026-04-22 09:46:24 +0300

    promqna v tipa ot class na enum na vsichki klasove v employee/domain/enums

--- Employees/Domain/Enums/EmployeeStatus.cs
+++ Employees/Domain/Enums/EmployeeStatus.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CompanyHRManagementSystem.Employees.Domain.Enums
 {
-    public class EmployeeStatus
+    public enum EmployeeStatus
     {
     }
 }
--- Employees/Domain/Enums/EmploymentType.cs
+++ Employees/Domain/Enums/EmploymentType.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CompanyHRManagementSystem.Employees.Domain.Enums
 {
-    public class EmploymentType
+    public enum EmploymentType
     {
     }
 }
