5e389ca1a73937fdc8efa5084f8e1f058d9ae9b2
PCK207 <PCK207@K207>
2026-04-29 09:28:56 +0300

    dobavqne na klas CompanyStorage

---
+++ Infrastructure/CompanyStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using CompanyHRManagementSystem.Employees.Domain.Entities;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyHRManagementSystem.Employees.Infrastructure
+{
+    public class CompanyStorage
+    {
+    }
+}
