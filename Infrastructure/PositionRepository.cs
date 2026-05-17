6e10a378229643620782867e8422b49f14d3731f
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-17 21:28:24 +0300

    Positionrepository GetbyId and Get all implementirani

--- Infrastructure/PositionRepository.cs
+++ Infrastructure/PositionRepository.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using CompanyHRManagementSystem.Employees.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CompanyHRManagementSystem.Employees.Infrastructure
 {
-    public class PositionRepository : IPositionRepository
+    public class PositionRepository
     {
+        private readonly FileStorage _storage;
+
+        public PositionRepository(FileStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public Position GetById(int id)
+        {
+            var db = _storage.Load();
+
+            return db.Positions.FirstOrDefault(p => p.PositionId == id)
+                   ?? throw new Exception("Position not found");
+        }
+
+        public IReadOnlyList<Position> GetAll()
+        {
+            var db = _storage.Load();
+            return db.Positions;
+        }
+
+        
     }
 }
