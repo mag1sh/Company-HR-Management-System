60db0bcb4daf07f7d02332767896cfc81ac4fb86
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-17 21:07:04 +0300

    novi clasove

--- Application/Interfaces/ILeaveRepository.cs
+++ Application/Interfaces/ILeaveRepository.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using CompanyHRManagementSystem.Domain.Entities;
+using CompanyHRManagementSystem.Employees.Domain.Entities;
 
 namespace CompanyHRManagementSystem.Employees.Services.Interfaces
 {
     internal interface ILeaveRepository
     {
+        Leave GetById(int id);
+
+        IReadOnlyList<Leave> GetAll();
+
+        void Save(Leave leave);
+
+        void Delete(int id);
+
+        IReadOnlyList<Leave> GetByEmployeeId(int employeeId);
+
+        IReadOnlyList<Leave> GetByDepartmentId(int departmentId);
+
+        
     }
 }
