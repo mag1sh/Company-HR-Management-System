53c641fd0331f113a9f61670017ce16035ee9117
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-17 21:22:07 +0300

    SalaryRepository rename to FileSalaryRepository

--- Infrastructure/SalaryRepository.cs
+++ Infrastructure/SalaryRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CompanyHRManagementSystem.Employees.Infrastructure
 {
-    internal class SalaryRepository
+    internal class FileSalaryRepository
     {
     }
 }
