e5e4707af4b20a1659f51693b3cbb2141a8d99d6
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-27 17:54:25 +0300

    SalaryHistory e implementiran + constructor

--- Domain/Entities/SalaryHistory.cs
+++ Domain/Entities/SalaryHistory.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CompanyHRManagementSystem.Employees.Domain.Entities
+namespace Domain.Entities
 {
-    public class SalaryHistory : List<Salary>
+    public class SalaryHistory
     {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public decimal OldSalary { get; set; }
+
+        public decimal NewSalary { get; set; }
+
+        public DateTime ChangeDate { get; set; }
+
+        public string Reason { get; set; }
+
+        public SalaryHistory()
+        {
+        }
+
+        public SalaryHistory(int employeeId, decimal oldSalary, decimal newSalary, string reason)
+        {
+            EmployeeId = employeeId;
+            OldSalary = oldSalary;
+            NewSalary = newSalary;
+            Reason = reason;
+            ChangeDate = DateTime.Now;
+        }
     }
 }
