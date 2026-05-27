a7feded21736767790741495c85cab17eb86d3e9
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-27 18:08:27 +0300

    constructor na UI-a

--- ConsoleUI/HRConsoleUI.cs
+++ ConsoleUI/HRConsoleUI.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompanyHRManagementSystem.Employees.Services;
 
 namespace CompanyHRManagementSystem.Employees.ConsoleUI
 {
     public class HRConsoleUI
     {
-    }
+       
+            private readonly EmployeeService _employeeService;
+
+            public HRConsoleUI(EmployeeService employeeService)
+            {
+                _employeeService = employeeService;
+            }
+     }
+    
 }
