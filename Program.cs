6e343cedcf7be3326c52ed45c6743e164bebebc2
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-27 21:27:57 +0300

    maj dobavih packetite ot terminal ama moje i da ne sam

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
-﻿using CompanyHRManagementSystem.Employees.Infrastructure;
-using CompanyHRManagementSystem.Employees.Services.Interfaces;
-using CompanyHRManagementSystem.Employees.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompanyHRManagementSystem.Application;
 using CompanyHRManagementSystem.Employees.ConsoleUI;
+using CompanyHRManagementSystem.Employees.Infrastructure;
+using CompanyHRManagementSystem.Employees.Services;
+using CompanyHRManagementSystem.Employees.Services.Interfaces;
 
 
 namespace CompanyHRManagementSystem
 {
+    //maj dobavih packetite ama nz
     internal class Program
     {
         static void Main(string[] args)
         {
-            EmployeeService employeeService = new EmployeeService() ;
-            HRConsoleUI ui = new HRConsoleUI(employeeService);
+            
+            CompanyStorage sharedStorage = new CompanyStorage();
+ 
+            var employeeService = new EmployeeService(sharedStorage);
+            var leaveService = new LeaveService(sharedStorage);
+            var departmentService = new DepartmentService(sharedStorage);
+            var positionService = new PositionService(sharedStorage);
+
+            var ui = new HRConsoleUI(employeeService, departmentService, positionService, leaveService);
+
             ui.Start();
         }
     }
