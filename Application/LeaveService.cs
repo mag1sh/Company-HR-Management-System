29174b2c93988e448406d586a298db41d528c376
Sverlio <User@DESKTOP-OB1B2K1>
2026-05-27 18:07:49 +0300

    LeaveService

--- Application/LeaveService.cs
+++ Application/LeaveService.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompanyHRManagementSystem.Employees.Domain.Entities;
+using CompanyHRManagementSystem.Employees.Domain.Enums;
+using Services.Interfaces;
 
 namespace CompanyHRManagementSystem.Employees.Services
 {
-    internal class LeaveService
+    public class LeaveService : ILeaveService
     {
+        private readonly List<Leave> _leaves = new List<Leave>();
+
+        public void RequestLeave(Leave leave)
+        {
+            bool hasConflict = _leaves.Any(l =>
+                l.EmployeeId == leave.EmployeeId &&
+                l.Status == LeaveStatus.Approved &&
+                leave.StartDate <= l.EndDate &&
+                leave.EndDate >= l.StartDate);
+
+            if (hasConflict)
+                throw new Exception("Leave conflict detected");
+
+
+            _leaves.Add(leave);
+        }
+
+        public void ApproveLeave(int leaveId)
+        {
+            var leave = _leaves.FirstOrDefault(l => l.Id == leaveId);
+
+            if (leave == null)
+                throw new Exception("Leave not found");
+
+            leave.Approve();
+        }
+
+
+
+        public void RejectLeave(int leaveId)
+        {
+            var leave = _leaves.FirstOrDefault(l => l.Id == leaveId);
+
+            if (leave == null)
+                throw new Exception("Leave not found");
+
+            leave.Reject();
+        }
+
+        public List<Leave> GetAllLeaves()
+        {
+            return _leaves;
+        }
     }
 }
+
