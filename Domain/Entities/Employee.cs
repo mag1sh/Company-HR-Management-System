77427c792869ffd3f8b710f09e1ad65d64c0914d
Sverlio <PCK212@K212>
2026-05-29 09:22:11 +0300

    navigation properties v Employee

--- Domain/Entities/Employee.cs
+++ Domain/Entities/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using CompanyHRManagementSystem.Employees.Domain.Enums;
 using CompanyHRManagementSystem.Employees.Domain.ValueObjects;
+using Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace CompanyHRManagementSystem.Employees.Domain.Entities
 {
     public class Employee
     {
-        private static int _nextId = 1;
+       // private static int _nextId = 1;
         public int Id { get;  set; }
 
         public FullName Name { get;  set; }
@@ -26,9 +27,20 @@ namespace CompanyHRManagementSystem.Employees.Domain.Entities
         public int DepartmentId { get;  set; }
         public int PositionId { get;  set; }
 
+        public virtual Department Department { get; set; }
+        public virtual Position Position { get; set; }
+        public virtual Salary Salary { get; set; }
+
+        public virtual ICollection<Leave> Leaves { get; set; }
+        public virtual ICollection<EmploymentHistory> EmploymentHistories { get; set; }
+        public virtual ICollection<SalaryHistory> SalaryHistories { get; set; }
+
         // Constructor
         protected Employee()
         {
+            Leaves = new HashSet<Leave>();
+            EmploymentHistories = new HashSet<EmploymentHistory>();
+            SalaryHistories = new HashSet<SalaryHistory>();
         }
         public Employee( FullName name, Email email, PhoneNumber phoneNumber, Address address,
                         DateTime hireDate, int departmentId, int positionId)
