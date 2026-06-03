aec1ae2120bc19512638e426b64427e616438207
PCK207 <PCK207@K207>
2026-06-03 09:56:30 +0300

    Леки промени в класа

--- Domain/Entities/Department.cs
+++ Domain/Entities/Department.cs
@@ -8,32 +8,27 @@ namespace CompanyHRManagementSystem.Employees.Domain.Entities
 {
     public class Department
     {
-        
-           
-            public int DepartmentId { get; private set; }
+        public int DepartmentId { get; private set; }
 
-            public string Name { get; private set; }
-            public string Description { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
 
-          public  ICollection<Employee> Employees { get; private set; }
+        public  ICollection<Employee> Employees { get; private set; }
 
 
-         // public List<Employee> Employees { get; private set; }
+        // public List<Employee> Employees { get; private set; }
 
-            protected Department()
-            {
-              
-            }
-
-            public Department(string name, string description)
+        protected Department()
         {
-               
-                Name = name;
-                Description = description;
-                Employees = new List<Employee>();
-            }
-     }
 
+        }
 
-    
+        public Department(string name, string description)
+        {
+
+            Name = name;
+            Description = description;
+            Employees = new List<Employee>();
+        }
+    }
 }
